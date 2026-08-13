d64e2433a3be22ed01a240dfa2abce815b0d78a7
Evelyn <evelyn.evangelista@edu.senai.br>
2026-08-13 11:05:17 -0300

    Exercío-Lista

--- Exercício9.js
+++ Exercício9.js
@@ -1,1 +1,7 @@
+function celsiusParaFahrenheit(c) {
+    let conversao = (c * 9 / 5 + 32);
+    return conversao;
+}
 
+let resultadoConvertido = celsiusParaFahrenheit(7);
+console.log(resultadoConvertido);
