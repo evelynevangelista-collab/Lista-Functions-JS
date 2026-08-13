702c07b92fae9715f9a58c0cfe4b21fa0615f603
Evelyn <evelyn.evangelista@edu.senai.br>
2026-08-13 11:52:51 -0300

    Exercício-Lista

---
+++ Exercício13.js
@@ -0,0 +1,9 @@
+function verificarPar(numero) {
+    if (numero % 2 === 0) {
+        return "Par";
+    } else {
+        return "Impar";
+    }
+}
+console.log(verificarPar(3));
+console.log(verificarPar(7));
