6e661706588f074ca8baa69ac96317af781aa834
Evelyn <evelyn.evangelista@edu.senai.br>
2026-08-20 16:23:39 -0300

    metade

---
+++ Exercício14.js
@@ -0,0 +1,16 @@
+function calcularMedia(nota1, nota2) {
+    return(nota1 + nota2) / 2;
+}
+function verificarAprovacao(media) {
+    if (media >=6) {
+        return "Aprovado";
+        }else {
+            return "Reprovado";
+        }
+}
+
+let media = calcularMedia(3, 8);
+let resultado = verificarAprovacao(media);
+
+console.log(media);
+console.log(resultado);
---
+++ Exercício15.js
@@ -0,0 +1,7 @@
+function classificarNota(nota) {
+  if (nota < 10 ||nota >= 9) {
+    return "Exelente";
+  }else if{
+    
+  }
+}
