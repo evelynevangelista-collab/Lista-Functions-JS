7b0ef5de528d01dc84825833bb020b83be2bfd29
Evelyn <evelyn.evangelista@edu.senai.br>
2026-08-25 13:36:16 -0300

    Exercícios-lista

--- Exercício15.js
+++ Exercício15.js
@@ -1,7 +1,17 @@
 function classificarNota(nota) {
-  if (nota < 10 ||nota >= 9) {
-    return "Exelente";
-  }else if{
-    
+  if (nota === 10 || nota === 9) {
+    return "Excelente";
+  } else if (nota === 7 || nota === 8) {
+    return "Bom";
+  } else if (nota === 6) {
+    return "Aprovado"; 
+    } else { 
+      return "reprovado";
+    }
+
   }
-}
+
+  console.log(classificarNota(7));
+  console.log(classificarNota(4));
+  console.log(classificarNota(9));
+  console.log(classificarNota(6));
